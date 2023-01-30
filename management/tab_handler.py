d7d1f5e57f703e28d7e6758c3383122ea945b299
Felipe Holanda <felipe.nether@icloud.com>
2023-01-30 00:17:24 -0300

    Fix test errors

--- management/tab_handler.py
+++ management/tab_handler.py
@@ -1,3 +1,4 @@
+from menu import products
 from management.product_handler import get_product_by_id
 
 
@@ -5,10 +6,10 @@ def calculate_tab(param: list[dict]) -> (float):
     try:
         if not type(param) == list:
             raise TypeError("You must pass a list to PARAM param.")
-        total = 0
+        total = 0.0
         for item in param:
             product = get_product_by_id(item['_id'])
             total += product['price'] * item['amount']
-        return total
+        return {"subtotal": f"${round(total, 2)}"}
     except TypeError as e:
         raise e
